3cc45ff027132ff94e0fc64f6b17d014bd12e2ad
Ademar Vazquez <ademarvazquez53@gmail.com>
2025-12-12 05:16:45 +0000

    crea tailwind.config.js
    Tu archivo debe verse así:
    
    /** @type {import('

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        sidebar: "hsl(var(--sidebar))",
+        "sidebar-foreground": "hsl(var(--sidebar-foreground))",
+        "sidebar-border": "hsl(var(--sidebar-border))",
+        "sidebar-accent": "hsl(var(--sidebar-accent))",
+        "sidebar-accent-foreground":
+          "hsl(var(--sidebar-accent-foreground))",
+      },
+    },
+  },
+  plugins: [],
+};
