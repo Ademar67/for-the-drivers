381e0123fc6a616afcd4e8c4bf8399f3712f12db
Ademar Vazquez <ademarvazquez53@gmail.com>
2026-03-13 20:44:22 +0000

    improve native pwa splash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#0054A6"
+  themeColor: "#0054A6",
+  width: "device-width",
+  initialScale: 1
 };
 
 export default function RootLayout({
